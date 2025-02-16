e7c1cff5f062c59076779a8ffc73cfd313426b18
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2025-02-16 15:58:34 -0600

    EndPoint_Home

---
+++ Lista_tareas/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, HTTPException
+from typing import Optional
+from datetime import date
+
+app = FastAPI(
+    title="API_Tareas",
+    description="API para gestionar tareas",
+    version="1.0.1"
+)
+
+tareas = [
+    {"id": 1, "titulo": "tarea_1", "descripcion": "hacer un resumen", "vencimiento": "2025-02-17", "estado": "activo"},
+    {"id": 2, "titulo": "tarea_2", "descripcion": "investigar", "vencimiento": "2025-03-10", "estado": "activo"},
+    {"id": 3, "titulo": "tarea_3", "descripcion": "leer un libro", "vencimiento": "2025-04-05", "estado": "activo"},
+    {"id": 4, "titulo": "tarea_4", "descripcion": "hacer ejercicio", "vencimiento": "2025-05-01", "estado": "activo"},
+    {"id": 5, "titulo": "tarea_5", "descripcion": "practicar código", "vencimiento": "2025-06-20", "estado": "activo"},
+]
+
+# EndPoint home
+@app.get('/', tags=['Inicio'])
+def home():
+    """Endpoint de bienvenida a la API."""
+    return {"hello": "Hello FastAPI!"}
+
