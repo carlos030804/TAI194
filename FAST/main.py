30f1fd7e35f84fecd2c926f76d74aba1f82e0ccc
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2025-02-21 15:22:39 -0600

    avance_practica5

--- FAST/main.py
+++ FAST/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
-from typing import Optional
+from typing import Optional, List
+from pydantic import BaseModel
 
 app = FastAPI(
     title="Mi primer API",
@@ -7,12 +8,20 @@ app = FastAPI(
     version="1.0.1"
 )
 
+
+class modelUsuario(BaseModel):
+    id: int
+    nombre: str
+    edad: int 
+    correo: str 
+    
+    
 usuarios=[
-	{"id":1,"nombre":"carlos", "edad":20},
-	{"id":2,"nombre":"Paulina", "edad":24},
-	{"id":3,"nombre":"Saul", "edad":21},
-	{"id":4,"nombre":"Maye", "edad":26},
-	{"id":5,"nombre":"Yasmin", "edad":42},
+	{"id":1,"nombre":"carlos", "edad":20, "correo":"122041770@upq.edu.mx"},
+	{"id":2,"nombre":"Paulina", "edad":24, "correo":"122041771@upq.edu.mx"},
+	{"id":3,"nombre":"Saul", "edad":21, "correo":"122041772@upq.edu.mx"},
+	{"id":4,"nombre":"Maye", "edad":26, "correo":"122041773@upq.edu.mx"},
+	{"id":5,"nombre":"Yasmin", "edad":42, "correo":"122041774@upq.edu.mx"},
 ]
 
 #EndPoint home
@@ -21,26 +30,26 @@ def home():
 	return {'hello':'Hello FastAPI!'}
 
 #EndPoint Consulta todos
-@app.get('/todosusuarios',tags=['Operaciones CRUD'])
+@app.get('/todosusuarios', response_model=List[modelUsuario], tags=['Operaciones CRUD'])
 def leer():
-	return {'Usuarios Rguistrados':usuarios}
+	return usuarios
 
 #EndPoint para post
-@app.post('/Usuarios/',tags=['Operaciones CRUD'])
-def guardar(usuario:dict):
+@app.post('/Usuarios/',response_model=modelUsuario,tags=['Operaciones CRUD'])
+def guardar(usuario:modelUsuario):
     for usr in usuarios:
-            if usr ["id"] == usuario.get("id"):
+            if usr ["id"] == usuario.id:
                 raise  HTTPException(status_code=400,detail="El usuario ya existe")
     
     usuarios.append(usuario)
     return usuario 
 
 #EndPoint para actualizar
-@app.put('/Usuarios/{id}',tags=['Operaciones CRUD'])
-def actualizar(id:int,usuarioActualizado:dict):
+@app.put('/Usuarios/{id}',response_model=modelUsuario,tags=['Operaciones CRUD'])
+def actualizar(id:int,usuarioActualizado:modelUsuario):
     for index, usr in enumerate(usuarios):
         if usr["id"] == id:
-            usuarios[index].update(usuarioActualizado)
+            usuarios[index]=usuarioActualizado.model_dump()
             return usuarios[index]
     
     raise HTTPException(status_code=400,detail="El usuario no existe")
--- Lista_tareas/main.py
+++ Lista_tareas/main.py
@@ -10,7 +10,7 @@ app = FastAPI(
 
 tareas = [
     {"id": 1, "titulo": "tarea_1", "descripcion": "hacer un resumen", "vencimiento": "2025-02-17", "estado": "activo"},
-    {"id": 2, "titulo": "tarea_2", "descripcion": "investigar", "vencimiento": "2025-03-10", "estado": "activo"},
+    {"id":  2, "titulo": "tarea_2", "descripcion": "investigar", "vencimiento": "2025-03-10", "estado": "activo"},
     {"id": 3, "titulo": "tarea_3", "descripcion": "leer un libro", "vencimiento": "2025-04-05", "estado": "activo"},
     {"id": 4, "titulo": "tarea_4", "descripcion": "hacer ejercicio", "vencimiento": "2025-05-01", "estado": "activo"},
     {"id": 5, "titulo": "tarea_5", "descripcion": "practicar código", "vencimiento": "2025-06-20", "estado": "activo"},
