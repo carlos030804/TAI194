b46c26c5568d3ec648bb99a01da2429424a988f4
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2025-03-19 18:12:31 -0600

    Practica_7_Avance

--- FAST/main.py
+++ FAST/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.responses import JSONResponse
 from typing import Optional, List
-from models import modelUsuario, modelAuth
+from FAST.modelsPydantic import modelUsuario, modelAuth
 from gentoken import createToken
 from middlewares import BearerJWT
 
